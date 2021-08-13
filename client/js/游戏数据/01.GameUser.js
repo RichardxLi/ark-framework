e7fb82537c3908d9f6d0e3f4dbb2f1c90d72f6d2
lix <lix@huitone.com>
2021-08-13 13:48:49 +0800

    fix

--- client/js/游戏数据/01.GameUser.js
+++ client/js/游戏数据/01.GameUser.js
@@ -3,8 +3,9 @@
  * 当前用户账号相关
  */
 function GameUser() {
-    this.name = "";
-    this.isHost = false;
-    this.isVisitor = false;
-    this.inRoom = false;
+    this.name = "";         // 用户名
+    this.isHost = false;    // 是否为主机
+    this.isVisitor = false; // 是否为旁观者
+    this.inRoom = false;    // 是否在房间中
+    this.playerIndex = 0;   // 玩家序号 GamePlayers内部使用
 };

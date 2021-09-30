2cecdd15cb6facea5fd90d4a770959f886497e75
lixiang <richardli744@gmail.com>
2021-09-30 09:29:42 +0800

    fix

--- server/src/main/main.go
+++ server/src/main/main.go
@@ -11,26 +11,16 @@ func main() {
 	registerServer.Start(8080)
 	fmt.Printf("注册服务器启动[%d]\n", 8080)
 
-	go func() {
-		for {
-			select {
+	//循环
+	for{
+		select {
 			case port := <-tcp.StartChan:
 				tcp.RunEchoServer(port)
 				fmt.Printf("广播服务器启动[%d]\n", port)
-			}
-		}
-	}()
-
-	go func() {
-		for {
-			select {
 			case port := <-tcp.CloseChan:
 				tcp.CloseEchoServer(port)
 				fmt.Printf("广播服务器关闭[%d]\n", port)
-			}
 		}
-	}()
+	}
 
-	//循环
-	select {}
 }
